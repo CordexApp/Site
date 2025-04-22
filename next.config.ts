eb62e04314f53d3cbde88af1c25bd907b6cfcdec
Shaan Fulton <shaanfulton@wifi-10-40-36-6.wifi.berkeley.edu>
2025-04-21 17:13:58 -0700

    Resolve Node DNS caching issues in containers

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,20 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  // Add hostname configuration
+  experimental: {
+    serverComponentsExternalPackages: ["sharp"],
+  },
+  // Avoid Node DNS caching issues in containers
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.watchOptions = {
+        ...config.watchOptions,
+        poll: 500,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
