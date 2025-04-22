d774cd0b4fa2169ba21b28a0b675cc175a7ba44b
Shaan Fulton <shaanfulton@135-180-244-251.fiber.dynamic.sonic.net>
2025-04-22 13:39:53 -0700

    Resolve next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",
+  serverExternalPackages: ["sharp"],
+  experimental: {},
+  // Avoid DNS resolution issues in containers
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.watchOptions = {
+        ...config.watchOptions,
+        poll: 500,
+      };
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
